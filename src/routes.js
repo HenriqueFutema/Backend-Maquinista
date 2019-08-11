3489b55866122cdae5d061e74fbab0faa1cbe879
HenriqueFutema <minifut0407@gmail.com>
2019-08-11 19:32:27 -0300

    middleware auth

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const app = express()
 const routes = express.Router()
+
+const authMiddleware = require('./middleware/auth')
 
 const UserController = require('./controllers/UserController')
 const SessionController = require('./controllers/SessionController')
@@ -7,5 +10,7 @@ const SessionController = require('./controllers/SessionController')
 
 routes.post('/signup', UserController.store)
 routes.post('/signin', SessionController.store)
+
+app.use(authMiddleware)
 
 module.exports = routes
