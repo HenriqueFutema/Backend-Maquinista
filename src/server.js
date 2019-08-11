160ef393a9b70d32052b6d0c2e171f28b0a5e751
HenriqueFutema <minifut0407@gmail.com>
2019-08-11 18:34:14 -0300

    Routes separado

--- src/server.js
+++ src/server.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send("AAAA")
-})
+app.use(require('./routes'))
 
 app.listen(3000, () => {
     console.log('Porta 3000');
