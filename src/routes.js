f3c78aa1acfbd030fc1e619e5beb16699609a900
HenriqueFutema <minifut0407@gmail.com>
2019-08-12 13:47:06 -0300

    Todas as routes em routes.js

--- src/routes.js
+++ src/routes.js
@@ -2,7 +2,15 @@ const express = require("express");
 const routes = express.Router();
 
 const UserController = require("./controllers/UserController");
+const SessionController = require("./controllers/SessionController");
 const ChatController = require("./controllers/ChatController");
+
+const authMiddleware = require("./middleware/auth");
+
+routes.post("/signup", UserController.store);
+routes.post("/signin", SessionController.store);
+
+routes.use(authMiddleware);
 
 routes.get("/users", UserController.index);
 routes.post("/message", ChatController.store);
--- src/server.js
+++ src/server.js
@@ -1,31 +1,18 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
-const authMiddleware = require('./middleware/auth')
-const database = require('./config/database')
-
-const UserController = require('./controllers/UserController')
-const SessionController = require('./controllers/SessionController')
+const database = require("./config/database");
 
 mongoose.connect(database.url, {
     useNewUrlParser: true
-})
+});
 
+app.use(bodyParser.json());
 
-app.use(bodyParser.json())
-
-
-app.post('/signup', UserController.store)
-app.post('/signin', SessionController.store)
-
-app.use(authMiddleware)
-
-//Rotas Depois do Auth
-app.use(require('./routes'))
+app.use(require("./routes"));
 
 app.listen(3000, () => {
-    console.log('Porta 3000');
-
-})
+    console.log("Porta 3000");
+});
