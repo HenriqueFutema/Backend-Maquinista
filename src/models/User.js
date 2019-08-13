43a32b640c3c05477ac4e3b6ce89793b807fdbb6
HenriqueFutema <minifut0407@gmail.com>
2019-08-13 19:06:25 -0300

    chat no Model do User

--- src/models/User.js
+++ src/models/User.js
@@ -1,8 +1,7 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
-const authConfig = require('../config/auth')
-
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth");
 
 const UserSchema = mongoose.Schema({
     name: {
@@ -16,29 +15,30 @@ const UserSchema = mongoose.Schema({
     password: {
         type: String,
         require: true
-    }
-})
+    },
+    chat: [{ type: mongoose.Schema.Types.ObjectId, ref: "Message" }]
+});
 
-UserSchema.pre('save', async function(next) {
-    if (!this.isModified('password')) {
-        return next()
+UserSchema.pre("save", async function(next) {
+    if (!this.isModified("password")) {
+        return next();
     }
 
-    this.password = await bcrypt.hash(this.password, 8)
-})
+    this.password = await bcrypt.hash(this.password, 8);
+});
 
 UserSchema.methods = {
     compareHash(password) {
-        return bcrypt.compare(password, this.password)
+        return bcrypt.compare(password, this.password);
     }
-}
+};
 
 UserSchema.statics = {
     generateToken({ id }) {
         return jwt.sign({ id }, authConfig.secret, {
             expiresIn: authConfig.ttl
-        })
+        });
     }
-}
+};
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", UserSchema);
