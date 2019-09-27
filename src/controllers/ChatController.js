a1fa22a4262e295c9177cdb99c903c379ed92cbf
HenriqueFutema <minifut0407@gmail.com>
2019-09-26 21:34:07 -0300

    Controle de sessões

--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -4,14 +4,21 @@ module.exports = {
     async store(req, res) {
         const { text } = req.body;
 
-        const session_id = await watson.createSession({
-            assistant_id: "cbacde9f-0add-44c6-b4a6-cc41af707e04"
-        });
+        if (!req.body.session_id) {
+            const session_id = await watson.createSession({
+                assistant_id: "cbacde9f-0add-44c6-b4a6-cc41af707e04"
+            });
+            session = session_id.session_id;
+        } else {
+            session = req.body.session_id;
+        }
+
+        console.log(session);
 
         watson
             .message({
                 assistant_id: "cbacde9f-0add-44c6-b4a6-cc41af707e04",
-                session_id: session_id.session_id,
+                session_id: session,
                 input: {
                     message_type: "text",
                     text: text
